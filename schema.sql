56ecbe4e8b81fa03b0b1f64c98260c409237f432
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-20 17:43:21 -0500

    Completed table schemas, add primary and foreign keys

--- schema.sql
+++ schema.sql
@@ -4,23 +4,30 @@ CREATE DATABASE departments_db;
 \c departments_db;
 
 CREATE TABLE department_agg (
-    id SERIAL,
-    name VARCHAR(30) PRIMARY KEY
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role_agg (
-    id SERIAL, 
-    title VARCHAR(30) PRIMARY KEY,
+    id SERIAL PRIMARY KEY, 
+    title VARCHAR(30),
     salary DECIMAL,
-    department INTEGER 
+    department INTEGER,
+    FOREIGN KEY (department)
+    REFERENCES department_agg(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee_agg(
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
     manager_id INTEGER,
-    FOREIGN KEY (manager_id)
+    FOREIGN KEY (role_id)
+    REFERENCES role_agg(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY manager_id
     REFERENCES employee_agg(id)
+    ON DELETE SET NULL
 );
