337de5cbf30f27712ee76c99fedff389e6468592
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-20 17:27:25 -0500

    Updating repo

--- schema.sql
+++ schema.sql
@@ -13,12 +13,14 @@ CREATE TABLE role_agg (
     title VARCHAR(30) PRIMARY KEY,
     salary DECIMAL,
     department INTEGER 
-)
+);
 
-CREATE TABLE (
+CREATE TABLE employee_agg(
     id SERIAL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
-    manager_id INTEGER
-)
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee_agg(id)
+);
