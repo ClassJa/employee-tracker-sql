3f867c08160eac9e15e46f53f01caa12eb1a02f1
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-23 17:18:17 -0500

    Refactoring the files and debugging

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { type } = require('os')
 // const pg = require("postgres")
 const { Pool } = require('pg') 
 const { exit } = require('process')
+// const pool = require('./config')
 // const tables = require('')
 
 
@@ -17,18 +18,18 @@ const pool = new Pool(
     console.log("Connected to the departments_db database: ")
     // , pool.database
 )
-// why won't this work?
+
 
 
 pool.connect()
 
-const allDepartments = "View All Departments"
-const allRoles = "View All Roles"
-const allEmployees = "View All Employees"
-const addDepartment = "Add Department"
-const addRole = "Add Role"
-const addEmployee = "Add Employee"
-const quitProgram =  "Exit Program"
+const DisplayDepartments = "View All Departments"
+const DisplayRoles = "View All Roles"
+const DisplayEmployees = "View All Employees"
+const AddDepartment = "Add Department"
+const AddRole = "Add Role"
+const AddEmployee = "Add Employee"
+const EndProgram =  "Exit Program"
 
 const questions = [
         {
@@ -43,102 +44,153 @@ const questions = [
 function init(){
     // asks main prompts continuously
     inquirer.prompt(questions).then((response) => {
+        switch (response.action) {
+            case DisplayDepartments:
+                displayDepartments(response)
+            break;
+            case DisplayRoles:
+                displayRoles(response)
+            break;
+            case DisplayEmployees:
+                displayEmployees(response)
+            break;
+            case AddDepartment:
+                 addNewDepartment()
+            break;
+            case AddRole:
+                addNewRole()
+            break;
+            case AddEmployee:
+                addNewEmployee()
+            break;
+            case EndProgram:
+                exitProgram()
+                break;
+        }
         // add switch statement, running specififc functions on user input 
-        displayDepartments(response)
-        displayRoles(response)
-        displayEmployees(response)
-        addNewDepartment(response)
-        addNewRole(response)
-        addNewEmployee(response)
-        exitProgram(response)
-        console.log(response)
+        // displayDepartments(response)
+        // displayRoles(response)
+        // displayEmployees(response)
+        // addNewDepartment()
+        // addNewRole()
+        // addNewEmployee()
+        // exitProgram()
+        // console.log({response})
     })
 }
 
 
 function displayDepartments(arg1) {
     const tableName = 'department_agg'
-    if(arg1.selection1 === allDepartments) {
+    if(arg1.selection1 === DisplayDepartments) {
         pool.query(`SELECT * FROM ${tableName}`, (err, {rows}) => {
             if (err) {
                 console.error(err)
             }
             console.table(rows)
-            init()
+            // init()
         })
     }
 }
 
 function displayRoles(arg1) {
     const tableName = 'role_agg'
-    if(arg1.selection1 === allRoles) {
+    if(arg1.selection1 === DisplayRoles) {
         pool.query(`SELECT * FROM ${tableName}`, (err, {rows}) => {
             if(err) {
                 console.error(err)
             } 
             console.table(rows)
-            init()
+            // init()
         })
     }
 }
 
 function displayEmployees(arg1) {
     const tableName = 'employee_agg'
-    if(arg1.selection1 === allEmployees) {
+    if(arg1.selection1 === DisplayEmployees) {
         pool.query(`SELECT * FROM ${tableName}`, (err, {rows}) => {
             if(err) {
                 console.error(err)
             }
             console.table(rows)
-            init()
+            // init()
         })
     }
 }
 
-function addNewDepartment(arg1){
+ function addNewDepartment(){
     const tableName = 'department_agg'
-    if(arg1.selection1 === addDepartment) {
-        inquirer.prompt([
-            {
+    const question = [
+        {
             message: "What is the name of the department you want to add?",
-            name: "addedDepartment",
-            type: "input"
+            name: "addedDepartment"
         }
-    ]).then((response) => {
- // Debug why the prompt is being invoked
- pool.query(`INSERT INTO ${tableName} (name) VALUES ('${response}')`, (err, {rows, fields}) => {
-    if(err){
-        console.error(err)
-    } else {
-        console.table(rows)
-        console.log(arg1.addedDepartment)
-        console.log(response)
-        console.log("Department added!", rows)
+    ]
+
+    inquirer.prompt(question)
+    .then(({addedDepartment}) => {
+        pool.query(`INSERT INTO ${tableName} (name) VALUES ('${addedDepartment}')`)
+        .then(() => {
+            console.log("Department added!")
+            // init()
+        })
+        })
     }
-})
-})
-}
-}
+    
 
-
-
-
-function addNewRole(arg1){
+function addNewRole(){
     const tableName = 'role_agg'
-    console.log(arg1)
-    if(arg1 === addRole) {
-        pool.query(`INSERT INTO ${tableName}`, (err) => {
-        })
-    }
+    const question = [
+        {
+            message: "What is the name of the role you want to add?",
+            name: "addedRole"
+        },
+        {
+            message: "What is the salary of the role you want to add?",
+            name: "addedSalary"
+        },
+        {
+            message: "What is the department of the role you want to add?",
+            name: "addedRoleDepartment"
+        }
+    ]
+    inquirer.prompt(question)
+    .then(({addedRole}) => {
+        pool.query(`INSERT INTO ${tableName} (title, salary, department) VALUES ('${addedRole}, ')`)
+    })
+    .then(() => {
+        console.log("Role Added!")
+        // init()
+    })
 }
 
-function addNewEmployee(arg1){
+function addNewEmployee(){
     const tableName = 'employee_agg' 
-    if(arg1 === addEmployee) {
-        pool.query(`INSERT INTO ${tableName}`, (err) => {
-        })
+    
+    const question = [
+        {
+            message: "What is the first name of the employee you want to add?",
+            name: "firstName"
+        },
+        {
+            message: "What is the last name of the employee you want to add?",
+            name: "lastName"
+        },
+        {
+            message: "What is the role id of the employee you want to add?",
+            name: "roleId"
+        }
+    ]
+    inquirer.prompt(question)
+    .then(({addedEmployee}) => {
+        pool.query(`INSERT INTO ${tableName} (first_name, last_name, role_id) VALUES ('${addedEmployee.firstName}, ${addedEmployee.lastName}, ${addedEmployee.roleId}')`)
+    })
+    .then(() => {
+        console.log("Employee Added!")
+        // init()
+    })
     }
-}
 
 // Debug this
 function exitProgram(arg1) {
