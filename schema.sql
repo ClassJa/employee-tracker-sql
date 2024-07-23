296e9ab5c8d3f3b08bba460103f0d06e54d5936a
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-23 15:39:40 -0500

    Debugging

--- schema.sql
+++ schema.sql
@@ -1,34 +1,34 @@
-DROP DATABASE IF EXISTS departments_db;
-CREATE DATABASE departments_db;
+-- DROP DATABASE IF EXISTS departments_db;
+-- CREATE DATABASE departments_db;
 
-\c departments_db;
+-- \c departments_db;
 
-CREATE TABLE department_agg (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE department_agg (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(30) NOT NULL
+-- );
 
-CREATE TABLE role_agg (
-    id SERIAL PRIMARY KEY, 
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department INTEGER NOT NULL,
-    FOREIGN KEY (department)
-    REFERENCES department_agg(id)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE role_agg (
+--     id SERIAL PRIMARY KEY, 
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL NOT NULL,
+--     department INTEGER NOT NULL,
+--     FOREIGN KEY (department)
+--     REFERENCES department_agg(id)
+--     ON DELETE SET NULL
+-- );
 
-CREATE TABLE employee_agg (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER DEFAULT NULL,
-    -- Is this correct?
-    FOREIGN KEY (role_id)
-    REFERENCES role_agg(id)
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee_agg(id)
-    ON DELETE SET NULL
-);
+-- CREATE TABLE employee_agg (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INTEGER NOT NULL,
+--     manager_id INTEGER DEFAULT NULL,
+--     -- Is this correct?
+--     FOREIGN KEY (role_id)
+--     REFERENCES role_agg(id)
+--     ON DELETE SET NULL,
+--     FOREIGN KEY (manager_id)
+--     REFERENCES employee_agg(id)
+--     ON DELETE SET NULL
+-- );
